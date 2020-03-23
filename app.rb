82004875641248467519330e2fed6a72e0d51daf
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-23 23:02:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,9 +6,19 @@ class App < Sinatra::Base
     erb :index
   end
 
+<<<<<<< HEAD
   post '/checkout' do
     @session = session
     item = params["item"]
     @session[:item] = item
   end
+=======
+
+    post '/checkout' do
+      @sessions = session
+      item = params["item"]
+      @sessions[:item] = item
+    end
+
+>>>>>>> 1704cead1fcfdc134637952f067f865988169ceb
 end
